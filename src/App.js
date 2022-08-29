338e4f64e767f3cd13b561f8acc4b1c0b9f66a31
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2022-08-29 17:18:13 -0600

    getting started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
-function App() {
+import React from 'react';
+import './App.css'
 
+function App() {
 return (
-    <div classname="app">
-    Testing
-    Testing
+    <div classname="App">
+        <h3>Welcome to:</h3>
+        <h1>All Hallows High</h1>
+        <p>Monster kinds most prestegeous school of Tradition and Conservaion of Halloween Traditions </p>
     </div>
 )
 };
